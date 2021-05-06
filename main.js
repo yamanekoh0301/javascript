10c6c5307ae3b5ed2357c7cb8f9da9f4b596569b
yamanekoh0301 <yamanekoh0301@yahoo.co.jp>
2021-05-06 17:57:54 +0900

    25まで終了（Arrayとアロー関数）

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-const myMap = new Map();
+const fruits = [
+    "apple", "banana"
+];
 
-myMap.set("id", 3);
-myMap.set("name","honda");
+console.log(fruits);
 
-console.log(myMap);
-console.log(myMap.get("name"));
+fruits.push("orange")
 
-const valueList = myMap.values();
+console.log(fruits);
 
-for (v of valueList){
-    console.log(v);
-}
+// callback（無名関数）
+fruits.forEach(function(input){
+    console.log(input);
+})
+
+console.log("===========================================")
+
+// アロー関数（ES6から）
+// 関数名あり
+const getItem = () => {console.log("allow")}
+getItem()
+
+console.log("===========================================")
+
+// 関数名なし
+fruits.forEach(input => console.log(input));
+
+console.log("===========================================")
+
+console.log("<<Array.filter>>")
+// Array.filter
+const scores = [10,20,30,40];
+const newScores = scores.filter(value => value >= 30);
+console.log(newScores);
+
+console.log("<<Array.find>>")
+// Array.find
+const members = ["honda", "kagawa", "nagatomo"];
+const member = members.find(value => value === "nagatomo");
+console.log(member);
+
+console.log("<<Array.map>>")
+// Array.map 配列をもとに新しい配列を作成
+const userList = [10,20,30,40];
+const userIdList = userList.map(value => `user_${value}`);
+console.table(userIdList);
 
