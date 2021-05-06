395e39d8c833f53433718cd7a7c14f924cc26e27
yamanekoh0301 <yamanekoh0301@yahoo.co.jp>
2021-05-06 15:49:32 +0900

    22まで終了（Mapオブジェクト）

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const test = "test";
-const test2 = " test2 ";
+const myMap = new Map();
 
-console.log(test.concat(test2));
-console.log(test2.trim());
-console.log(test2.substring(0,3));
-console.log(test2.substring(3));
-console.log(test2.replace("2","3"));
+myMap.set("id", 3);
+myMap.set("name","honda");
+
+console.log(myMap);
+console.log(myMap.get("name"));
+
+const valueList = myMap.values();
+
+for (v of valueList){
+    console.log(v);
+}
+
