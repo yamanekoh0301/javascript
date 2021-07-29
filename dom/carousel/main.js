fda4d2ca8e48f1bbf7690364dd4e7ed40fdeb2f5
yamanekoh0301 <yamanekoh0301@yahoo.co.jp>
2021-07-29 15:08:34 +0900

    44まで終了（カルーセル）

--- dom/carousel/main.js
+++ dom/carousel/main.js
@@ -10,27 +10,58 @@ const images = [
   'images/image006.jpg',
 ];
 
-let currentindex = 1;
+let currentIndex = 0;
 
 const mainImage = document.getElementById("carousel__main");
-mainImage.src = images[currentindex];
+mainImage.src = images[currentIndex];
 // console.log(mainImage.src)
 
+// pythonのenumerateに類似
 for(let [index, image] of images.entries()){
   // console.log(index, image);
 
   const img = document.createElement('img');
   img.src = image;
-  console.log(img.src)
+  // console.log(img.src)
 
   const li = document.createElement('li');
-  if (index === currentindex) {
+  if (index === currentIndex) {
     li.classList.add('current');
   }
+
+  li.addEventListener('click',()=>{
+    mainImage.src = image;
+    mainImage.classList.add('active');
+
+  setTimeout(() => {
+    mainImage.classList.remove('active');
+  }, 800); //ミリ秒 1000=1秒
+
+  const thumbnails = document.querySelectorAll('.carousel__thumbnails > li');
+  thumbnails[currentIndex].classList.remove('current');
+  currentIndex = index;
+  thumbnails[currentIndex].classList.add('current');
+  });
 
   li.appendChild(img);
   document.querySelector('.carousel__thumbnails').appendChild(li);
 
-
-
 }
+
+const next = document.getElementById('carousel__next');
+next.addEventListener('click', () => {
+  let target = currentIndex + 1;
+  if (target === images.length){
+    target = 0;
+  }
+  document.querySelectorAll('.carousel__thumbnails > li')[target].click();  
+});
+
+const prev = document.getElementById('carousel__prev');
+prev.addEventListener('click', () => {
+  let target = currentIndex - 1;
+  if (target < 0){
+    target = images.length - 1;
+  }
+  document.querySelectorAll('.carousel__thumbnails > li')[target].click();
+});
