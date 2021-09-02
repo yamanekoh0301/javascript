d7c07f5c298c77dd4c47fdfd33ca50ca3112d606
yamanekoh0301 <yamanekoh0301@yahoo.co.jp>
2021-09-02 16:55:35 +0900

    49まで終了(ファイルアップロード・プレビュー)

--- dom/forms/main.js
+++ dom/forms/main.js
@@ -7,7 +7,19 @@ console.log(formTestYourName);
 const formTestYourNameValue = document.forms.form_test1.your_name.value;
 console.log(formTestYourNameValue);
 
-formTestYourName.addEventListener('input', ()=>{
+formTestYourName.addEventListener('input',()=>{
     let yourNameValue = document.getElementById("your_name_value");
     yourNameValue.textContent = formTestYourName.value;
-})
+});
+
+const preview = document.forms.form_test1.preview;
+
+document.forms.form_test1.image_file.addEventListener('change', e =>{
+    // console.log(e.target.files[0]);
+    const uploadFile = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load',()=>{
+        preview.src = reader.result;
+    });
+    reader.readAsDataURL(uploadFile);
+});
